e56c65cfb9aa57c9428e30a09fd442c728d7c870
Brian Wang <bwang@avetta.com>
2016-10-20 10:58:38 -0700

    Refactored to Ruby code style conventions

--- rb/tunlr.rb
+++ rb/tunlr.rb
@@ -2,26 +2,26 @@
 require 'json'
 
 # Read in app ports
-tuleshome = ENV['TULES_HOME']
-confpath = "#{tuleshome}/etc/ports.json"
+tules_home = ENV['TULES_HOME']
+conf_path = "#{tules_home}/etc/ports.json"
 
 # Check for ConfigFile
-hasConfig = File.exist?(confpath)
+has_config = File.exist?(conf_path)
 
-puts confpath if hasConfig
-f = File.read(confpath) if hasConfig
-ports = JSON.parse(f) if hasConfig
+puts conf_path if has_config
+f = File.read(conf_path) if has_config
+ports = JSON.parse(f) if has_config
 
 host = ARGV.shift
-hostport = ARGV.shift
+host_port = ARGV.shift
 # If it's not literal port, look for it in config
-if hostport =~ /[A-Za-z]/
-  hostport = ports[hostport] if hasConfig
+if host_port =~ /[A-Za-z]/
+  host_port = ports[host_port] if has_config
 end
-portprefix = ARGV.shift
+port_prefix = ARGV.shift
 
 # Fetch ip from config
-hostip = `cat ~/.ssh/config | grep #{host}$ -A 3 | grep Hostname | awk '{print $2}'`.strip
+host_ip = `cat ~/.ssh/config | grep #{host}$ -A 3 | grep Hostname | awk '{print $2}'`.strip
 
-puts tunnel = "ssh -N -L #{portprefix}#{hostport}:#{hostip}:#{hostport} #{host}"
+puts tunnel = "ssh -N -L #{port_prefix}#{host_port}:#{host_ip}:#{host_port} #{host}"
 puts `#{tunnel}`
