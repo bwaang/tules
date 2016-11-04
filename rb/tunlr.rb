8b4b76a202f5fa1283a635341513a69be98ff69f
Brian Wang <bwang@avetta.com>
2016-11-04 11:24:53 -0700

    Adding environment configurations

--- rb/tunlr.rb
+++ rb/tunlr.rb
@@ -5,7 +5,7 @@ require 'json'
 tules_home = ENV['TULES_HOME']
 conf_path = "#{tules_home}/etc/ports.json"
 
-# Check for ConfigFile
+# Check for ConfigFile existence
 has_config = File.exist?(conf_path)
 
 puts conf_path if has_config
@@ -13,11 +13,13 @@ f = File.read(conf_path) if has_config
 ports = JSON.parse(f) if has_config
 
 host = ARGV.shift
+# if host exists in config use that one
+host = ports['envs'][host] if ports['envs'].has_key? host and has_config
+
 host_port = ARGV.shift
 # If it's not literal port, look for it in config
-if host_port =~ /[A-Za-z]/
-  host_port = ports[host_port] if has_config
-end
+host_port = ports[host_port] if host_port =~ /[A-Za-z]/ and has_config
+
 port_prefix = ARGV.shift
 
 # Fetch ip from config
